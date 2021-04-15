9990c69f771ee4fc09168a0a8a99f74093e44ea9
EC2 Default User <korethis0816@gmail.com>
2021-04-15 06:37:52 +0000

    [Update]4-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,25 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @books = @user.books
   end
 
   def edit
+    @user = User.new
+    @user = User.find(params[:id])
+    @user = User.new(user_params)
+    @user.user_id = current_user.id
+    @user.save
+    redirect_to user_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image_id)
   end
 end
