cb9dce42780317cf282e347cb3cde03ca3e7bfd8
EC2 Default User <korethis0816@gmail.com>
2021-04-17 03:11:31 +0000

    [Update]4-10

--- Gemfile
+++ Gemfile
@@ -65,6 +65,3 @@ gem 'devise'
 
 gem 'refile', require:"refile/rails", github:"manfe/refile"
 gem 'refile-mini_magick'
-
-gem 'refile', require:"refile/rails", github:'manfe/refile'
-gem 'refile-mini_magick'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,28 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @user = current_user
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = current_user
     @books = @user.books
   end
 
   def edit
     @user = User.new
     @user = User.find(params[:id])
-    @user = User.new(user_params)
-    @user.user_id = current_user.id
-    @user.save
-    redirect_to user_path
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :introduction, :profile_image_id)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
