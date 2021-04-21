0f57061f66694fb52d63ce5d804e945ec5b69390
EC2 Default User <korethis0816@gmail.com>
2021-04-21 05:11:33 +0000

    [Update]4-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.new
-    @user = current_user
+    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user.id)
+    end
   end
 
   def update
