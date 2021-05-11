b9d1395841544d3a20bc1eb2f58d159522560be2
EC2 Default User <korethis0816@gmail.com>
2021-05-11 12:37:00 +0000

    [Update]

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,15 @@ class User < ApplicationRecord
   attachment :profile_image
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: following_id
+  has_many :followings, through: :active_relationships, source: :follower
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: follower_id
+  has_many :followers, through: :passive_relationships, source: :following
+
+  def followed_by?(user)
+    followed.find_by(following_id: user.id).present?
+  end
+
 
   validates :name,  uniqueness: true, length: {in: 2..20}
   validates :introduction, length: {maximum: 50}
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ Rails.application.routes.draw do
     resource :favorites, only:[:create, :destroy]
     resources :comments, only:[:create, :destroy]
   end
-  resources:users, only:[:index, :show, :edit, :update]
+  resources:users, only:[:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get :follows, on: :member
+    get :followers, on: :member
+  end
   get '/search' => 'search#search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
